fe68721fc607aea0001e85492588cde96577c229
oliver <oliversanz1991@gmail.com>
2021-05-09 13:19:23 +0200

    Empty project

---
+++ app/src/main/java/com/oliversanz/marvel/android/herolist/HeroListActivity.kt
@@ -0,0 +1,12 @@
+package com.oliversanz.marvel.android.herolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oliversanz.marvel.R
+
+class HeroListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hero_list)
+    }
+}
